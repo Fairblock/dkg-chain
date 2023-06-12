072dd20ee3f9d5fb4c4d51a59bd7d2c748314ad0
SetarehGhorshi <ssghorsh@uwaterloo.ca>
2023-06-12 16:43:45 -0400

    latest

--- x/dkg/keeper/msg_server_file_dispute.go
+++ x/dkg/keeper/msg_server_file_dispute.go
@@ -2,6 +2,9 @@ package keeper
 
 import (
 	"context"
+	"strconv"
+	"strings"
+
 	//"crypto/cipher"
 
 	//"crypto/cipher"
@@ -20,10 +23,12 @@ import (
 	"crypto/sha256"
 	"fmt"
 	"math/big"
-	"strings"
+
+	//"strings"
 
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	//bls12381 "github.com/kilic/bls12-381"
 
 	vsskyber "github.com/FairBlock/vsskyber"
@@ -86,18 +91,46 @@ func initInput(numOfUsers int, threshInPercent float64) (threshold int, PublicAn
 
 	return threshold, PublicAndPrivateKeyArray, nil
 }
+type P struct{
+	Concat string 
+	G []byte
+	Pkj []byte
+	Pki []byte
+	Ek []byte
+	C []byte
+	R []byte
+	cNew []byte
+	X1t []byte
+	Y1t []byte
+	T1P []byte
+	commits [][]byte
+}
+func VerifyProof(pointG, publicKeyI, publicKeyJ, encryptionKeyIJ kyber.Point, c []byte, r kyber.Scalar, cReal []byte) bool {
+	reverseBytes(c)
+	c2KyberScalar := bls.NewKyberScalar()
+	c2KyberScalar.UnmarshalBinary(c)
+	//c2KyberScalar := bls.NewKyberScalar().SetBytes(c)
 
-func VerifyProof(pointG, publicKeyJ, publicKeyI, encryptionKeyIJ kyber.Point, c []byte, r kyber.Scalar) bool {
+//	x1ToR := bls.NewBLS12381Suite().G1().Point().Base()
+//	y1ToC := bls.NewBLS12381Suite().G1().Point().Base()
+//	t1Prime := bls.NewBLS12381Suite().G1().Point().Base()
+suite := bls.NewBLS12381Suite()
+x1ToR :=  suite.G1().Point()
 
-	c2KyberScalar := bls.NewKyberScalar().SetBytes(c)
+x1ToR.Mul(r,pointG) 
+g , _ := pointG.MarshalBinary()
+//cb , _ := c2KyberScalar.MarshalBinary()
+pkj,_ := publicKeyJ.MarshalBinary()
+pki, _ := publicKeyI.MarshalBinary()
+ek , _ := encryptionKeyIJ.MarshalBinary()
+// rr,_:= r.MarshalBinary()
 
-	x1ToR := bls.NewBLS12381Suite().G1().Point().Base()
-	y1ToC := bls.NewBLS12381Suite().G1().Point().Base()
-	t1Prime := bls.NewBLS12381Suite().G1().Point().Base()
-
-	x1ToR.Mul(r, pointG)
-	y1ToC.Mul(c2KyberScalar, publicKeyJ)
-	t1Prime.Add(x1ToR, y1ToC)
+// x1t,_ := x1ToR.MarshalBinary()
+// panic(P{G:g,R:rr,X1t: x1t})
+	//x1ToR := bls.NewBLS12381Suite().G1().Point().Mul(r,pointG)
+	y1ToC := bls.NewBLS12381Suite().G1().Point().Mul(c2KyberScalar, publicKeyJ)
+	t1Prime := bls.NewBLS12381Suite().G1().Point().Add(x1ToR, y1ToC)
+	//x1t,_ := y1ToC.MarshalBinary()
 
 	x2ToR := bls.NewBLS12381Suite().G1().Point().Base()
 	y2ToC := bls.NewBLS12381Suite().G1().Point().Base()
@@ -106,35 +139,75 @@ func VerifyProof(pointG, publicKeyJ, publicKeyI, encryptionKeyIJ kyber.Point, c 
 	x2ToR.Mul(r, publicKeyI)
 	y2ToC.Mul(c2KyberScalar, encryptionKeyIJ)
 	t2Prime.Add(x2ToR, y2ToC)
+	t1p, _ :=t1Prime.MarshalBinary()
+	t2p,_ := t2Prime.MarshalBinary()
+	//concat := bytes.Join([][]byte{g, pkj,pki, ek, t1p, t2p}, []byte{})
+	//s := concat
+	
+	//y1t,_ := y1ToC.MarshalBinary()
+	
+	
+	gstr := make([]string, len(g))
+	for i, b := range g {
+		gstr[i] = strconv.Itoa(int(b))
+	}
+	pkjstr := make([]string, len(pkj))
+	for i, b := range pkj {
+		pkjstr[i] = strconv.Itoa(int(b))
+	}
+	pkistr := make([]string, len(pki))
+	for i, b := range pki {
+		pkistr[i] = strconv.Itoa(int(b))
+	}
+	ekstr := make([]string, len(ek))
+	for i, b := range ek {
+		ekstr[i] = strconv.Itoa(int(b))
+	}
+	t1pstr := make([]string, len(t1p))
+	for i, b := range t1p {
+		t1pstr[i] = strconv.Itoa(int(b))
+	}
+	t2pstr := make([]string, len(t2p))
+	for i, b := range t2p {
+		t2pstr[i] = strconv.Itoa(int(b))
+	}
 
-	concat := []string{pointG.String(), publicKeyJ.String(), publicKeyI.String(), encryptionKeyIJ.String(), t1Prime.String(), t2Prime.String()}
-	s := strings.Join(concat, "")
+	s := "[" + strings.Join(gstr, ", ") + "]"+"[" + strings.Join(pkjstr, ", ") + "]"+"[" + strings.Join(pkistr, ", ") + "]"+"[" + strings.Join(ekstr, ", ") + "]"+"[" + strings.Join(t1pstr, ", ") + "]"+"[" + strings.Join(t2pstr, ", ") + "]"
+	//panic(P{C:c,cNew: t1p})
+//	concat := []string{string(g), string(pkj), string(pki), string(ek), string(t1p), string(t2p)}
+//	s := strings.Join(concat, "")
 	h := sha256.New()
 	h.Write([]byte(s))
 	cPrime := h.Sum(nil)
-
-	return bytes.Equal(cPrime, c)
+    //panic(P{C: cReal ,cNew:cPrime})
+	return bytes.Equal(cPrime, cReal)
 
 }
 
-func handleDispute(listOfComplaints []listOfComplaints) {
+// func handleDispute(listOfComplaints []listOfComplaints) {
 
-	for i := 0; i < len(listOfComplaints); i++ {
-		verifyResult := VerifyProof(PointG, listOfComplaints[i].pubKeyOfAccuser, listOfComplaints[i].pubKeyOfAccusee, listOfComplaints[i].kij, listOfComplaints[i].proof.hash, listOfComplaints[i].proof.scalar)
-		if !verifyResult {
-			fmt.Println("Accuser is malicous")
-			// slash the accuser goes here
-		}
-		if vsskyber.VerifyShare(listOfComplaints[i].share, listOfComplaints[i].commit) {
-			fmt.Println("Accusee is malicious")
-			//slash the accusee goes here
-		} else {
-			fmt.Println("Accuser is malicious")
-			// slash the accuser
-		}
-	}
+// 	for i := 0; i < len(listOfComplaints); i++ {
+// 		verifyResult := VerifyProof(PointG, listOfComplaints[i].pubKeyOfAccuser, listOfComplaints[i].pubKeyOfAccusee, listOfComplaints[i].kij, listOfComplaints[i].proof.hash, listOfComplaints[i].proof.scalar)
+// 		if !verifyResult {
+// 			fmt.Println("Accuser is malicous")
+// 			// slash the accuser goes here
+// 		}
+// 		if vsskyber.VerifyShare(listOfComplaints[i].share, listOfComplaints[i].commit) {
+// 			fmt.Println("Accusee is malicious")
+// 			//slash the accusee goes here
+// 		} else {
+// 			fmt.Println("Accuser is malicious")
+// 			// slash the accuser
+// 		}
+// 	}
 
-	//the result can be a column added to the complaint list that indicates the result of the complaint
+// 	//the result can be a column added to the complaint list that indicates the result of the complaint
+// }
+func reverseBytes(data []byte) {
+    length := len(data)
+    for i := 0; i < length/2; i++ {
+        data[i], data[length-i-1] = data[length-i-1], data[i]
+    }
 }
 func (k msgServer) FileDispute(goCtx context.Context, msg *types.MsgFileDispute) (*types.MsgFileDisputeResponse, error) {
 	 ctx := sdk.UnwrapSDKContext(goCtx)
@@ -144,7 +217,7 @@ func (k msgServer) FileDispute(goCtx context.Context, msg *types.MsgFileDispute)
 	// src := make([]byte, len(msg.Dispute.AddressOfAccusee))
 	// r.XORKeyStream(dst, src)
 
-	var slashed []byte
+	var slashed string
 	var dispute = types.Dispute{
 		AddressOfAccuser: msg.Dispute.AddressOfAccuser,
 		AddressOfAccusee: msg.Dispute.AddressOfAccusee,
@@ -154,6 +227,9 @@ func (k msgServer) FileDispute(goCtx context.Context, msg *types.MsgFileDispute)
 		CZkProof:         msg.Dispute.CZkProof,
 		RZkProof:         msg.Dispute.RZkProof,
 		Id:               msg.Dispute.Id,
+		AccuserId: msg.Dispute.AccuserId,
+		FaulterId: msg.Dispute.FaulterId,
+		CReal: msg.Dispute.CReal,
 	}
 
 	count := k.GetDisputeCount(ctx)
@@ -174,44 +250,55 @@ func (k msgServer) FileDispute(goCtx context.Context, msg *types.MsgFileDispute)
 	addOfAccuser.UnmarshalBinary(dispute.AddressOfAccuser)
 	
 	
-
-	rScalar := bls.NewKyberScalar().SetBytes(dispute.RZkProof)
-
-	res := VerifyProof(PointG, addOfAccusee, addOfAccuser, secretKeyIJ, dispute.CZkProof, rScalar)
+	reverseBytes(dispute.RZkProof)
+	rScalar := bls.NewKyberScalar()
+	rScalar.UnmarshalBinary(dispute.RZkProof)
+	//rs, _ := rScalar.MarshalBinary()
+	
+	res := VerifyProof(PointG, addOfAccusee, addOfAccuser, secretKeyIJ, dispute.CZkProof, rScalar, dispute.CReal)
 
 	if res {
 		//slash the accusee
-		commits := new([]kyber.Point)
+		commits := make([]kyber.Point,0)
 		for i := 0; i < len(dispute.Commit.Commitments); i++ {
-			(*commits)[i] = suite.G1().Point()
-			(*commits)[i].UnmarshalBinary(dispute.Commit.Commitments[i])
+			
+			c := suite.G1().Point()
+			c.UnmarshalBinary(dispute.Commit.Commitments[i])
+			commits = append(commits,c)
 		}
-		index := new(kyber.Scalar)
-		(*index).SetBytes(dispute.Share.Index)
-		value := new(kyber.Scalar)
-		(*value).SetBytes(dispute.Share.Value)
-		verify := vsskyber.VerifyShare(vsskyber.Share{Index: *index, Value: *value },*commits)
+	
+	//reverseBytes(dispute.Share.Index)
+	reverseBytes(dispute.Share.Value)
+	sharei := bls.NewKyberScalar()
+	sharei.SetInt64(int64(binary.BigEndian.Uint32(dispute.Share.Index)+1))
+	sharev := bls.NewKyberScalar()
+	sharev.UnmarshalBinary(dispute.Share.Value)
+	//panic(P{Concat: sharev.String(),C:dispute.Share.Index,R:dispute.Share.Value, commits: dispute.Commit.Commitments})		
+		verify := vsskyber.VerifyShare(vsskyber.Share{Index: sharei, Value: sharev },commits)
 		if !verify{
-		slashed = dispute.AddressOfAccusee}
+			//panic("accusee")
+		slashed = string(rune(dispute.FaulterId))}
 		if verify {
+			//panic("accuser 1")
 			//slash the accuser
-			slashed = dispute.AddressOfAccuser
+			slashed = string(rune(dispute.AccuserId))
 		}
 	}
 
 	if !res {
+		//panic("accuser 2")
 		//slash the accuser
-		slashed = dispute.AddressOfAccuser
+		slashed = string(rune(dispute.AccuserId))
 	}
 	event := sdk.NewEvent(
 		types.EventTypeKeygen,
-		sdk.NewAttribute(types.AttributeValueDispute, string(slashed)),
+		sdk.NewAttribute(types.AttributeValueDispute, slashed),
 		sdk.NewAttribute("keyID", msg.KeyId),
 		sdk.NewAttribute("from", msg.Creator),
 		sdk.NewAttribute("module", "dkg"),
 	)
 	ctx.EventManager().EmitEvent(event)
-	return &types.MsgFileDisputeResponse{Verdict: res, IdOfSlashedValidator: slashed}, nil
+	return &types.MsgFileDisputeResponse{Verdict: res, IdOfSlashedValidator: []byte(slashed)}, nil
 	//return nil, nil
 }
 
